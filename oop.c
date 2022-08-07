964424093966d07dd6298d89b9b489c73a4302be
Spawnzer <78969151+Spawnzer@users.noreply.github.com>
2022-08-07 12:39:32 -0400

    Create oop.c

---
+++ oop.c
@@ -0,0 +1,55 @@
+#include "oop.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int addi(int a, int b)
+{
+	return a+b;
+}
+
+int subs(int a, int b)
+{
+	return a-b;
+}
+
+int mult(int a, int b)
+{
+	return a*b;
+}
+
+int divi(int a, int b)
+{
+	return a/b;
+}
+
+OPERATION_VTABLE *init_vtable(fcn test)
+{
+	OPERATION_VTABLE *vtable;
+
+	vtable->add = addi;
+	vtable->div = divi;
+	vtable->sub = subs;
+	vtable->mul = mult;
+	return vtable;
+}
+
+fcn *get_fcn(void)
+{
+	static fcn test;
+	if (test.init != 1)
+	{
+		test.vtable = init_vtable(test);
+		test.init = 1;
+		test.res = 0;
+	}
+	return (&test);
+}
+
+int main(void)
+{
+	printf("res is %d \n", get_fcn()->vtable->add(20, 5));
+	printf("res is %d \n", get_fcn()->vtable->sub(20, 5));
+	printf("res is %d \n", get_fcn()->vtable->mul(20, 5));
+	printf("res is %d \n", get_fcn()->vtable->div(20, 5));
+
+}
